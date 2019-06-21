2e44e6d6015c01662e7232995f91030c01fc782f
afandi <science.afandi@gmail.com>
2019-06-21 17:25:40 +0700

    fix command factory

--- app/DOSBox/Command/CommandFactory.php
+++ app/DOSBox/Command/CommandFactory.php
@@ -7,12 +7,9 @@ use DOSBox\Command\Library\CmdCd as CmdCd;
 use DOSBox\Command\Library\CmdDir as CmdDir;
 use DOSBox\Command\Library\CmdMkDir as CmdMkDir;
 use DOSBox\Command\Library\CmdMkFile as CmdMkFile;
-<<<<<<< HEAD
 use DOSBox\Command\Library\CmdExit as CmdExit;
-=======
 use DOSBox\Command\Library\CmdHelp as CmdHelp;
 use DOSBox\Command\Library\CmdVer as CmdVer;
->>>>>>> 6726e1bfdd857361d296df97e9ab4352b92b30a4
 
 class CommandFactory {
     private $commands = array();
@@ -25,12 +22,9 @@ class CommandFactory {
         array_push($this->commands, new CmdMkDir("md", $drive));
         array_push($this->commands, new CmdMkFile("mkfile", $drive));
         array_push($this->commands, new CmdMkFile("mf", $drive));
-<<<<<<< HEAD
         array_push($this->commands, new CmdExit("exit", $drive));
-=======
         array_push($this->commands, new CmdHelp("help", $drive));
         array_push($this->commands, new CmdVer("ver", $drive));
->>>>>>> 6726e1bfdd857361d296df97e9ab4352b92b30a4
 
         // Add your commands here
     }
